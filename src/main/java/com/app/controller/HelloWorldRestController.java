6177418dda75149d8e8f8d72b2051535896d0bfe
treyturley <81778542+treyturley@users.noreply.github.com>
2021-09-29 16:39:45 -0500

    Fixed bug in HW controller.

--- src/main/java/com/app/controller/HelloWorldRestController.java
+++ src/main/java/com/app/controller/HelloWorldRestController.java
@@ -2,8 +2,6 @@ package com.app.controller;
 
 import java.time.LocalDateTime;
 
-import javax.print.attribute.standard.DateTimeAtCompleted;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +15,7 @@ public class HelloWorldRestController {
 	@RequestMapping("/")
 	public String sayHello() {
 		logger.info("Hello World Rest Controller Triggered at " + LocalDateTime.now());
-		return "Hello world Rest Controller docker test123. Time is: " + + LocalDateTime.now();
+		return "Hello world Rest Controller docker test123. Time is: " + LocalDateTime.now();
 	}
 
 }
